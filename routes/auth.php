822576011ead28d1ba19b37e1094f1da5f5e90b3
Eduardo Bessa <eduubessa@gmail.com>
2025-07-17 10:35:04 +0100

    Fix conflit merge on auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-<<<<<<< HEAD
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
@@ -36,25 +35,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
-
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
-=======
 //    Route::get('auth/sign-up', [RegisteredUserController::class, 'create'])->name('register');
 //    Route::post('auth/sign-up', [RegisteredUserController::class, 'store']);
     Route::get('auth/sign-in', [AuthenticatedSessionController::class, 'create'])->name('login');
@@ -91,5 +71,4 @@ Route::middleware('auth')->group(function () {
     });
 
     Route::post('/auth/sign-out', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
->>>>>>> feature/customers
 });
